94fd165d4bf08dc4887e2388fcb5b38307790d1e
raunakmishra18 <mraunak255@gmail.com>
2024-09-17 13:04:43 +0530

    hey

---
+++ practices/grade.java
@@ -0,0 +1,22 @@
+package practices;
+import java.util.Scanner;
+
+public class grade {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("grade calculator\n");
+        System.out.println("plese enter your marks precentage");
+        int marks = input.nextInt();
+        if (marks >= 90){
+            System.out.println("your grade is A");
+        }else if (marks >= 75) {
+            System.out.println("your grade is B");
+        }else if (marks >= 60){
+            System.out.println("your gradenis C");
+        }else if(marks >= 30){
+            System.out.println("your grade is D");
+        }else{
+            System.out.println("your grade is F");
+        }
+    }
+}
---
+++ practices/greaternumber.java
@@ -0,0 +1,24 @@
+package practices;
+import java.util.Scanner;
+
+public class greaternumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("welometo find greater number\n");
+        System.out.print("enter first number");
+        int num1 = input.nextInt();
+        System.out.print("enter secound number");
+        int num2 = input.nextInt();
+        System.out.print("enter third number");
+        int num3 = input.nextInt();
+
+
+        if(num1>=num2 && num1>=num3){
+            System.out.println("the greater number is" + num1);
+        }else if(num2>=num3){
+            System.out.println(num2 + "is the greater number");
+        }else{
+            System.out.println(num3 + "is the greater number");
+        }
+    }
+}
