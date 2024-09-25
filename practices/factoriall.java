21ac955ecfdbbd48d7d9d862ba676d3118d64278
raunakmishra18 <mraunak255@gmail.com>
2024-09-25 23:46:28 +0530

    september

---
+++ practices/factoriall.java
@@ -0,0 +1,26 @@
+package practices;
+import java.util.*;
+public class factoriall {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("factorial calculator");
+        System.out.println("enter the number");
+        int num = sc.nextInt();
+        int fact=factorial(num);
+        System.out.println(fact);
+
+    }
+
+    public static int factorial(int num){
+        if (num<2) {
+            return 1;
+        }
+        int fact=1;
+        int i = 2;
+        while (i<=num) {
+            fact=fact*i;
+            i++;
+        }
+        return fact;
+    }
+}
