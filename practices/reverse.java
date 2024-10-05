39ef2073781e0d605a81ede9752b7467b590f15c
raunakmishra18 <mraunak255@gmail.com>
2024-10-05 21:39:10 +0530

    5 oct

--- practices/reverse.java
+++ practices/reverse.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class reverse {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("welcome to reverse number");
+        System.out.println("enter the number");
+        int num = sc.nextInt();
+        int reverse=reverse(num);
+        System.out.println(reverse);
+
+    }
+    public static int reverse(int num){
+        int newNumber = 0;
+        while (num>0) {
+            int digit=num%10;
+            newNumber=newNumber*10+digit;
+            num/=10;
+        }
+        return newNumber;
     }
 }
