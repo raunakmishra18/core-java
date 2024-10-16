87652bb27b4caf8dad7bde75174ca2879c937410
raunakmishra18 <mraunak255@gmail.com>
2024-10-16 22:52:08 +0530

    hy

---
+++ practices/minmax.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class minmax {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int [] numarr=arrayutility.inputarray();
+        int max=max(numarr);
+        int min=min(numarr);
+
+    }
+    public static int max(int[] numarr){
+        if (numm arr.length){
+
+        }
+        return 0;
+    }
+    public static int min(int[] numarr){
+        return 0;
+    }
+}
